c6bb180881ac345b75662b1cd4a13447d10d8b1d
Daqota <38364934+Daqota@users.noreply.github.com>
2025-06-21 20:46:08 -0600

    added aabb_vs_aabb collision detection

--- game/utils.cpp
+++ game/utils.cpp
@@ -15,3 +15,9 @@ void move3d(Vector3* p, Vector3* v, Vector3 a, float dt) {
         *v = *v + a * dt;
     }
 }
+bool aabb_vs_aabb(Vector2 p1, float hs1x, float hs1y, Vector2 p2, float hs2x, float hs2y) { //returns true if the positions +/- the half size overlap
+    return p1.x + hs1x > p2.x - hs2x &&
+        p1.x - hs1x < p2.x + hs2x &&
+        p1.y + hs1y > p2.y - hs2y &&
+        p1.y - hs1y < p2.y + hs2y;
+}
