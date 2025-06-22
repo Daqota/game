cc0da0eb7784bafaaec5b61542b8abb8cdaf4b87
Daqota <38364934+Daqota@users.noreply.github.com>
2025-06-21 19:10:10 -0600

    simple movement funtions

--- game/utils.cpp
+++ game/utils.cpp
@@ -1,3 +1,17 @@
 #include "Vector2.cpp"
 #include "Vector3.cpp"
 #include "Vector4.cpp"
+
+//I'm gonna put some physichs stuff here for now
+void move2d(Vector2* p, Vector2* v, Vector2 a, float dt){
+    if (p && v) {
+        *p = *p + *v * dt;
+        *v = *v + a * dt;
+    }
+}
+void move3d(Vector3* p, Vector3* v, Vector3 a, float dt) {
+    if (p && v) {
+        *p = *p + *v * dt;
+        *v = *v + a * dt;
+    }
+}
